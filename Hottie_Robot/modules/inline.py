7eae97f6d89546390eaf7ae984b426224d42e579
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-11 18:15:28 +0530

    Create inline.py

--- Hottie_Robot/modules/inline.py
+++ Hottie_Robot/modules/inline.py
@@ -65,7 +65,7 @@ async def inline_query_handler(client, query):
 **Pyrogram:** `{pyrover}`
 **MongoDB:** `{mongover}`
 **Platform:** `{sys.platform}`
-**Profiles:** [BOT](t.me/Hottie_Robot)
+**Profiles:** [BOT](t.me/Hottie_Robot) ,parse_mode=ParseMode.MARKDOWN
 """,
                     photo_url="https://telegra.ph/file/7cab440852751e60368c1.jpg",
                     parse_mode="html",
