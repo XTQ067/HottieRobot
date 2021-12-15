c0839eb6489d224f5758e1455bda67b8ff56f1f7
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-15 18:17:31 +0700

    ok

--- Hottie_Robot/modules/__init__.py
+++ Hottie_Robot/modules/__init__.py
@@ -1,4 +1,6 @@
-from Hottie_Robot import LOAD, LOGGER, NO_LOAD
+import sys
+
+from Natsunagi import LOAD, LOGGER, NO_LOAD
 
 
 def __list_all_modules():
@@ -21,7 +23,7 @@ def __list_all_modules():
                 for mod in to_load
             ):
                 LOGGER.error("Invalid loadorder names. Quitting.")
-                quit(1)
+                sys.exit(1)
 
             all_modules = sorted(set(all_modules) - set(to_load))
             to_load = list(all_modules) + to_load
