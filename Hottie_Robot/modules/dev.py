537f8160707bc9207b81e30adeb2e49b5b32174b
aryazakaria01 <80420642+aryazakaria01@users.noreply.github.com>
2021-12-31 05:16:06 +0000

    Fixed the mistake code function

--- Hottie_Robot/modules/dev.py
+++ Hottie_Robot/modules/dev.py
@@ -4,7 +4,7 @@ import sys
 from time import sleep
 
 from telegram import TelegramError, Update
-from telegram.ext import CallbackContext, CommandHandler, run_async
+from telegram.ext import CallbackContext, CommandHandler
 
 from Hottie_Robot import dispatcher
 from Hottie_Robot.modules.helper_funcs.chat_status import dev_plus
@@ -56,7 +56,7 @@ def restart(update: Update, context: CallbackContext):
     os.execv("start.bat", sys.argv)
 
 
-LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True )
+LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True)
 GITPULL_HANDLER = CommandHandler("gitpull", gitpull, run_async=True)
 RESTART_HANDLER = CommandHandler("reboot", restart, run_async=True)
 
