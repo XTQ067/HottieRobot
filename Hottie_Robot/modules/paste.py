747b0b9b9ba79a365e19d2ad0298da3f1a0fd3d2
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2022-02-14 21:44:57 +0530

    Update paste.py

--- Hottie_Robot/modules/paste.py
+++ Hottie_Robot/modules/paste.py
@@ -1,81 +1,253 @@
+import asyncio
+import logging
 import os
+import sys
+import time
 
-import aiohttp
-from aiohttp import ClientResponseError, ServerTimeoutError, TooManyRedirects
-from Babe import aasf
-from pyrogram import filters
-from pyrogram.types import Message
+import spamwatch
+import telegram.ext as tg
+import telethon
+from aiohttp import ClientSession
+from ptbcontrib.postgres_persistence import PostgresPersistence
+from pyrogram import Client, errors
+from Python_ARQ import ARQ
+from redis import StrictRedis
+from telethon import TelegramClient
+from telethon.sessions import StringSession
 
-NEKOBIN_URL = "https://nekobin.com/"
-MAX_MSG_LENGTH = 4096
+from Hottie_Robot.utils.logger import log
 
-@aasf.on_message(filters.command(["paste"], ['/', ".", "?"]))
-async def nekobin_paste(_, message: Message):
-    """ Pastes the text directly to Nekobin  """
-    cmd = len(message.text)
-    msg = await message.reply("`Processing...`")
-    text = None
-    if message.text and cmd > 6:
-        _, args = message.text.split(maxsplit=1)
-        text = args
-    replied = message.reply_to_message
-    file_ext = '.txt'
-    if not cmd > 6 and replied and replied.document and replied.document.file_size < 2 ** 20 * 10:
-        file_ext = os.path.splitext(replied.document.file_name)[1]
-        path = await replied.download("downloads/")
-        with open(path, 'r') as d_f:
-            text = d_f.read()
-        os.remove(path)
-    elif not cmd > 6 and replied and replied.text:
-        text = replied.text
-    if not text:
-        await msg.edit("`input not found!`")
-        return
-    await msg.edit("`Pasting text...`")
-    async with aiohttp.ClientSession() as ses:
-        async with ses.post(NEKOBIN_URL + "api/documents", json={"content": text}) as resp:
-            if resp.status == 201:
-                response = await resp.json()
-                key = response['result']['key']
-                final_url = NEKOBIN_URL + key + file_ext
-                reply_text = f"**Nekobin** [URL]({final_url})"
-                await msg.edit(reply_text, disable_web_page_preview=True)
-            else:
-                await msg.edit("`Failed to reach Nekobin`")
+StartTime = time.time()
 
 
-@aasf.on_message(filters.command(["getpaste"], ['/', ".", "?"]))
-async def get_paste_(_, message: Message):
-    """ fetches the content of a Nekobin URL """
-    if message.text and len(message.text) == 9:
-        await message.reply("`input not found!`")
-        return
-    _, args = message.text.split(maxsplit=1)
-    link = args
-    msg = await message.reply("`Getting paste content...`")
-    if link.startswith(NEKOBIN_URL):
-        link = link[len(NEKOBIN_URL):]
-        raw_link = f'{NEKOBIN_URL}raw/{link}'
-    elif link.startswith("nekobin.com/"):
-        link = link[len("nekobin.com/"):]
-        raw_link = f'{NEKOBIN_URL}raw/{link}'
-    else:
-        await msg.edit("`Is that even a paste url?`")
-        return
-    async with aiohttp.ClientSession(raise_for_status=True) as ses:
-        try:
-            async with ses.get(raw_link) as resp:
-                text = await resp.text()
-        except ServerTimeoutError as e_r:
-            await msg.edit(f"`Request timed out -> {e_r}`")
-        except TooManyRedirects as e_r:
-            await msg.edit("`Request exceeded the configured `"
-                           f"`number of maximum redirections -> {e_r}`")
-        except ClientResponseError as e_r:
-            await msg.edit(f"`Request returned an unsuccessful status code -> {e_r}`")
-        else:
-            if len(text) > MAX_MSG_LENGTH:
-                await msg.edit("`Content Too Large...`")
-            else:
-                await msg.edit("--Fetched Content Successfully!--"
-                               f"\n\n**Content** :\n`{text}`")
+def get_user_list(__init__, key):
+    with open("{}/Hottie_Robot/{}".format(os.getcwd(), __init__), "r") as json_file:
+        return json.load(json_file)[key]
+
+
+# enable logging
+FORMAT = "[Hottie] %(message)s"
+logging.basicConfig(
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+    format=FORMAT,
+    datefmt="[%X]",
+)
+logging.getLogger("pyrogram").setLevel(logging.INFO)
+logging.getLogger("ptbcontrib.postgres_persistence.postgrespersistence").setLevel(
+    logging.WARNING
+)
+
+LOGGER = logging.getLogger("[Hottie]")
+LOGGER.info(
+    "Hottie Robot is starting. | An Kishore Project Parts. | Licensed under GPLv3."
+)
+LOGGER.info("Not affiliated to Tantei Wa Mou or Villain in any way whatsoever.")
+LOGGER.info("Project maintained by: github.com/AASFCYBERKING (t.me/AASFCYBERKING)")
+
+# if version < 3.9, stop bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 9:
+    LOGGER.error(
+        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+    )
+    sys.exit(1)
+
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+    TOKEN = os.environ.get("TOKEN", None)
+
+    try:
+        OWNER_ID = int(os.environ.get("OWNER_ID", None))
+    except ValueError:
+        raise Exception("Your OWNER_ID env variable is not a valid integer.")
+
+    JOIN_LOGGER = os.environ.get("JOIN_LOGGER", None)
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+
+    try:
+        DRAGONS = {int(x) for x in os.environ.get("DRAGONS", "").split()}
+        DEV_USERS = {int(x) for x in os.environ.get("DEV_USERS", "").split()}
+    except ValueError:
+        raise Exception("Your sudo or dev users list does not contain valid integers.")
+
+    try:
+        DEMONS = {int(x) for x in os.environ.get("DEMONS", "").split()}
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
+
+    try:
+        WOLVES = {int(x) for x in os.environ.get("WOLVES", "").split()}
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
+
+    try:
+        TIGERS = {int(x) for x in os.environ.get("TIGERS", "").split()}
+    except ValueError:
+        raise Exception("Your tiger users list does not contain valid integers.")
+
+    INFOPIC = bool(os.environ.get("INFOPIC", False))
+    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
+    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
+    URL = os.environ.get("URL", "")  # Does not contain token
+    PORT = int(os.environ.get("PORT", 5000))
+    CERT_PATH = os.environ.get("CERT_PATH")
+    API_ID = os.environ.get("API_ID", None)
+    APP_ID = os.environ.get("APP_ID", None)
+    API_HASH = os.environ.get("API_HASH", None)
+    APP_HASH = os.environ.get("APP_HASH", None)
+    BOT_ID = os.environ.get("BOT_ID", None)
+    DB_URL = os.environ.get("DATABASE_URL")
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    DONATION_LINK = os.environ.get("DONATION_LINK")
+    LOAD = os.environ.get("LOAD", "").split()
+    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
+    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
+    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
+    PMPERMIT = bool(os.environ.get("PMPERMIT", False))
+    STRING_SESSION = os.environ.get(
+        "STRING_SESSION", None
+    )  # Telethon Based String Session (2nd ID) [ From https://repl.it/@SpEcHiDe/GenerateStringSession ]
+    WORKERS = int(os.environ.get("WORKERS", 8))
+    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
+    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
+    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
+    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
+    AI_API_KEY = os.environ.get("AI_API_KEY", None)
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+    WALL_API = os.environ.get("WALL_API", None)
+    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
+    SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
+    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
+    REPOSITORY = os.environ.get("REPOSITORY", "")
+    REDIS_URL = os.environ.get("REDIS_URL")
+    IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
+    IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "hottierobot")
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+
+    try:
+        BL_CHATS = {int(x) for x in os.environ.get("BL_CHATS", "").split()}
+    except ValueError:
+        raise Exception("Your blacklisted chats list does not contain valid integers.")
+
+else:
+    from Hottie_Robot.config import Development as Config
+
+    TOKEN = Config.TOKEN
+
+    try:
+        OWNER_ID = int(Config.OWNER_ID)
+    except ValueError:
+        raise Exception("Your OWNER_ID variable is not a valid integer.")
+
+    JOIN_LOGGER = Config.JOIN_LOGGER
+    OWNER_USERNAME = Config.OWNER_USERNAME
+
+    try:
+        DRAGONS = {int(x) for x in Config.DRAGONS or []}
+        DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
+    except ValueError:
+        raise Exception("Your sudo or dev users list does not contain valid integers.")
+
+    try:
+        DEMONS = {int(x) for x in Config.DEMONS or []}
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
+
+    try:
+        WOLVES = {int(x) for x in Config.WOLVES or []}
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
+
+    try:
+        TIGERS = {int(x) for x in Config.TIGERS or []}
+    except ValueError:
+        raise Exception("Your tiger users list does not contain valid integers.")
+
+    EVENT_LOGS = Config.EVENT_LOGS
+    WEBHOOK = Config.WEBHOOK
+    URL = Config.URL
+    PORT = Config.PORT
+    CERT_PATH = Config.CERT_PATH
+    API_ID = Config.API_ID
+    API_ID = Config.APP_ID
+    API_HASH = Config.API_HASH
+    APP_HASH = Config.APP_HASH
+    BOT_ID = Config.BOT_ID
+
+    DB_URL = Config.SQLALCHEMY_DATABASE_URI
+    MONGO_DB_URI = Config.MONGO_DB_URI
+    DONATION_LINK = Config.DONATION_LINK
+    LOAD = Config.LOAD
+    NO_LOAD = Config.NO_LOAD
+    DEL_CMDS = Config.DEL_CMDS
+    STRICT_GBAN = Config.STRICT_GBAN
+    PMPERMIT = Config.PMPERMIT
+    STRING_SESSION = Config.STRING_SESSION
+    WORKERS = Config.WORKERS
+    BAN_STICKER = Config.BAN_STICKER
+    ALLOW_EXCL = Config.ALLOW_EXCL
+    CASH_API_KEY = Config.CASH_API_KEY
+    TIME_API_KEY = Config.TIME_API_KEY
+    OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
+    AI_API_KEY = Config.AI_API_KEY
+    REM_BG_API_KEY = Config.REM_BG_API_KEY
+    WALL_API = Config.WALL_API
+    YOUTUBE_API_KEY = Config.YOUTUBE_API_KEY
+    SUPPORT_CHAT = Config.SUPPORT_CHAT
+    SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
+    SPAMWATCH_API = Config.SPAMWATCH_API
+    INFOPIC = Config.INFOPIC
+
+    try:
+        BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
+    except ValueError:
+        raise Exception("Your blacklisted chats list does not contain valid integers.")
+
+DRAGONS.add(OWNER_ID)
+DEV_USERS.add(OWNER_ID)
+
+if not SPAMWATCH_API:
+    sw = None
+    LOGGER.warning("SpamWatch API key Is Missing! Recheck Your Config.")
+else:
+    try:
+        sw = spamwatch.Client(SPAMWATCH_API)
+    except:
+        sw = None
+        LOGGER.warning("Can't connect to SpamWatch!")
+
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+telethn = TelegramClient("HottieRobot", API_ID, API_HASH)
+pbot = Client("HottiePyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+aiohttpsession = ClientSession()
+# ARQ Client
+arq = ARQ("https://thearq.tech", "YIECCC-NAJARO-OLLREW-SJSRIP-ARQ", aiohttpsession)
+ubot = TelegramClient(StringSession(STRING_SESSION), APP_ID, APP_HASH)
+dispatcher = updater.dispatcher
+loop = asyncio.get_event_loop()
+
+DRAGONS = list(DRAGONS) + list(DEV_USERS)
+DEV_USERS = list(DEV_USERS)
+WOLVES = list(WOLVES)
+DEMONS = list(DEMONS)
+TIGERS = list(TIGERS)
+
+# Load at end to ensure all prev variables have been set
+from Hottie_Robot.modules.helper_funcs.handlers import (
+    CustomCommandHandler,
+    CustomMessageHandler,
+    CustomRegexHandler,
+)
+
+# make sure the regex handler can take extra kwargs
+tg.RegexHandler = CustomRegexHandler
+tg.CommandHandler = CustomCommandHandler
+tg.MessageHandler = CustomMessageHandler
